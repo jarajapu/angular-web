620243147941fc303184a7fa469788b8c0225387
Uday Jarajapu <uday.jarajapu@opower.com>
2013-09-07 21:58:59 -0700

    formatting, controls to edit/create profiles, bug fixes

--- bill-compare-mongo/project.js
+++ bill-compare-mongo/project.js
@@ -3,6 +3,8 @@ angular.module('project', ['mongolab']).
         $routeProvider.
           when('/list', {controller:ListCtrl, templateUrl:'list.html'}).
           when('/compare', {controller:BillCtrl, templateUrl:'compare.html'}).
+          when('/edit/:projectId', {controller:EditCtrl, templateUrl:'detail.html'}).
+          when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
           otherwise({redirectTo:'/compare'});
     });
 
@@ -18,6 +20,43 @@ function BillCtrl($scope, Project) {
       project_rand_one   = Math.floor(Math.random() * (max - min + 1)) + min;
       project_rand_two   = Math.floor(Math.random() * (max - min + 1)) + min;
 
+      if (project_rand_one === project_rand_two) {
+        project_rand_two   = Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+
       $scope.project_one = Project.query({sk: project_rand_one, l: '1'});
       $scope.project_two = Project.query({sk: project_rand_two, l: '1'});
+}
+
+function CreateCtrl($scope, $location, Project) {
+      $scope.save = function() {
+              Project.save($scope.project, function(project) {
+                        $location.path('/edit/' + project._id.$oid);
+                            });
+              }
+}
+
+function EditCtrl($scope, $location, $routeParams, Project) {
+      var self = this;
+
+      Project.get({id: $routeParams.projectId}, function(project) {
+                self.original = project;
+                    $scope.project = new Project(self.original);
+                      });
+
+      $scope.isClean = function() {
+                  return angular.equals(self.original, $scope.project);
+                    }
+
+      $scope.destroy = function() {
+                    self.original.destroy(function() {
+                              $location.path('/list');
+                                  });
+                      };
+
+      $scope.save = function() {
+                      $scope.project.update(function() {
+                                $location.path('/list');
+                                    });
+                        };
 }
